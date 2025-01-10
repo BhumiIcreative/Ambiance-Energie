f94e793a323c7926e16dac25ae247e04908793df
bhumiaktiv <bhumiaktiv@gmail.com>
2025-01-10 18:21:54 +0530

    add

--- amb_edf_prime/__manifest__.py
+++ amb_edf_prime/__manifest__.py
@@ -1,16 +1,12 @@
 # coding: utf-8
 {
-    'name': "Amb EDF Prime",
-
-    'summary': "Amb EDF Prime",
-
-    'description': "Amb EDF Prime",
-
-    'author': "Sirius-info",
-
-    'version': '17.0.0.0.0',
-    'application': True,
-
+    "name": "Amb EDF Prime",
+    "summary": "Amb EDF Prime",
+    "description": "Amb EDF Prime",
+    "author": "Aktiv software / Groupe OCI",
+    "version": "17.0.1.0.0",
+    "application": False,
+    "installable": True,
     # any module necessary for this one to work correctly
     'depends': [
         'sale_management',
@@ -18,18 +14,14 @@
         'account',
         'script_tools',
     ],
-
     # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-
-        'datas/product_template.xml',
-
-        'views/account_move_view.xml',
-        'views/sale_views.xml',
-
-        'wizard/wizard_add_edf_prime_view.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "datas/product_template.xml",
+        "views/account_move_view.xml",
+        "views/sale_views.xml",
+        "wizard/wizard_add_edf_prime_view.xml",
     ],
-
-    'license': 'LGPL-3',
+    "website": "http://www.aktivsoftware.com / https://www.oci.fr",
+    "license": "LGPL-3",
 }
